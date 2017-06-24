995106a3b8b12075b57105d9530daf02dfb17e8d
Henrique <henscalon@hotmail.com>
2017-06-24 18:54:57 -0300

    added session manager scs

--- app.go
+++ app.go
@@ -1,17 +1,24 @@
-
 package main
 
 import (
     "log"
     "net/http"
     "path/filepath"
+    "strconv"
+    "encoding/gob"
 
     "github.com/gorilla/mux"
+    "github.com/alexedwards/scs/engine/memstore"
+    "github.com/alexedwards/scs/session"
 )
+
+type User struct {
+	Folder string
+}
 
 type App struct {
     Router *mux.Router
-    RootFolder string
+    RootFolder string    
 }
 
 func (a *App) Initialize(rootFolder string) {
@@ -21,42 +28,87 @@ func (a *App) Initialize(rootFolder string) {
 }
 
 func (a *App) Run(addr string) {
-    log.Fatal(http.ListenAndServe(addr, a.Router))
+	gob.Register(User{})
+	
+	// initialize storage engine
+	engine := memstore.New(0)
+
+	// initialize session manager
+	sessionManager := session.Manage(engine,
+	    //session.IdleTimeout(30*time.Minute),
+	    session.ErrorFunc(sessionError),
+	)
+
+    log.Fatal(http.ListenAndServe(addr, sessionManager(a.Router)))
+}
+
+func sessionError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Println(err.Error())
+	respondWithError(w, http.StatusInternalServerError, "Sorry the application encountered an error")
 }
 
 func (a *App) initializeRoutes() {
+    a.Router.HandleFunc("/authenticate", a.authenticate).Methods("GET")
     a.Router.HandleFunc("/file", a.postFile).Methods("POST")
+    a.Router.HandleFunc("/import/{id:[0-9]+}", a.importFile).Methods("GET")
     a.Router.HandleFunc("/danfes/{folder}", a.getDanfes).Methods("GET")
+}
+
+func (a *App) authenticate(w http.ResponseWriter, r *http.Request) {
+	user := &User{}
+	err := session.PutObject(r, "user", user)
+	if err != nil {
+		log.Println(err.Error())
+		respondWithError(w, http.StatusInternalServerError, "Sorry the application encountered an error")
+	}	
 }
 
 func (a *App) postFile(w http.ResponseWriter, r *http.Request) {
     file, header, errFormFile := r.FormFile("file")
     if errFormFile != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid request payload: " + errFormFile.Error())
+    	log.Println("postFile, errFormFile: " + errFormFile.Error())
+        respondWithError(w, http.StatusBadRequest, "Invalid request payload")
         return
     }
     defer file.Close()
 
     folder, errUpload := uploadAndCreateFolder(file, header.Filename, a.RootFolder)
     if errUpload != nil {
-        respondWithError(w, http.StatusInternalServerError, "Error uploading file: " + errUpload.Error())
+    	log.Println("postFile, errUpload: " + errUpload.Error())
+        respondWithError(w, http.StatusInternalServerError, "Error uploading file")
         return
     }
 
     filePath := filepath.Join(folder, header.Filename)
     errUnzip := unzipFile(filePath, folder)
     if errUnzip != nil {
-        respondWithError(w, http.StatusInternalServerError, "Error unziping file: " + errUnzip.Error())
+    	log.Println("postFile, errUnzip: " + errUnzip.Error())
+        respondWithError(w, http.StatusInternalServerError, "Error unziping file")
         return
     }
 
     errRemove := removeFile(filePath)
     if errRemove != nil {
-        respondWithError(w, http.StatusInternalServerError, "Error removing file: " + errRemove.Error())
+    	log.Println("postFile, errUnzip: " + errRemove.Error())
+        respondWithError(w, http.StatusInternalServerError, "Error removing file")
         return
     }
 
+    // return the total files
+
     respondWithJSON(w, http.StatusCreated, nil)
+}
+
+func (a *App) importFile(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	_, err := strconv.Atoi(vars["id"]) // id
+	if err != nil {
+		log.Println("importFile: " + err.Error())
+        respondWithError(w, http.StatusBadRequest, "Invalid parameter")
+        return
+	}
+
+	// import file to db
 }
 
 func (a *App) getDanfes(w http.ResponseWriter, r *http.Request) {
